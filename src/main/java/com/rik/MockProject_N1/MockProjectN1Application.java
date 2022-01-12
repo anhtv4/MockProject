a11e5749bb1c0525370cfe3cecb687d41487a4ed
tv4nh <tva8320@gmail.com>
2022-01-12 22:37:49 +0700

    Initial commit

---
+++ src/main/java/com/rik/MockProject_N1/MockProjectN1Application.java
@@ -0,0 +1,13 @@
+package com.rik.MockProject_N1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockProjectN1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockProjectN1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rik/MockProject_N1/MockProjectN1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rik.MockProject_N1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockProjectN1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

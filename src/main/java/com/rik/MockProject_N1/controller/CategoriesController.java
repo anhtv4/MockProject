006f8bd1fd5584074266c010da93656a1241d3d8
mrsad9x <mr.sad.9x@gmail.com>
2022-01-13 15:54:59 +0700

    #1/finished crud category

--- src/main/java/com/rik/MockProject_N1/controller/CategoriesController.java
+++ src/main/java/com/rik/MockProject_N1/controller/CategoriesController.java
@@ -19,27 +19,27 @@ public class CategoriesController {
     CategoriesServiceImpl categoriesService;
 
     @GetMapping("/category")
-    public ResponseEntity<List<Categories>> getAllProduct(){
+    public ResponseEntity<List<Categories>> getAllCategories(){
         List<Categories> categoriesList = new ArrayList<>();
         categoriesList = categoriesService.findAllCategories();
         return new ResponseEntity<List<Categories>>(categoriesList, HttpStatus.OK);
     }
 
     @PostMapping("/category")
-    public ResponseEntity<String> addProduct(@RequestBody Categories categories){
+    public ResponseEntity<String> addCategory(@RequestBody Categories categories){
         categoriesService.addCategory(categories);
         return new ResponseEntity<String>("category add oki", HttpStatus.OK);
     }
 
     @DeleteMapping("/category")
-    public ResponseEntity<String> deleteProduct(@RequestParam Integer id){
+    public ResponseEntity<String> deleteCategory(@RequestParam Integer id){
         categoriesService.deleteCategory(id);
         return new ResponseEntity<String>("category: "+ id + " delete oki", HttpStatus.OK);
     }
     @PutMapping("/category")
-    public ResponseEntity<String> deleteProduct(@RequestParam String str, Integer id){
+    public ResponseEntity<String> deleteCategory(@RequestParam String str, Integer id){
 
         categoriesService.updateCategory(str, id);
-        return new ResponseEntity<String>("product name add: "+ str + " oki", HttpStatus.OK);
+        return new ResponseEntity<String>("rename title to: "+ str + " oki", HttpStatus.OK);
     }
 }

2006e48bc9da6d230754d9940d520e5827d6dc43
WangShon <wangshon0909@gmail.com>
2022-01-15 11:18:57 +0700

    Pushing User CRUD 2 times, and user login

--- src/main/java/com/rik/MockProject_N1/controller/admin/AdminController.java
+++ src/main/java/com/rik/MockProject_N1/controller/admin/AdminController.java
@@ -1,7 +1,5 @@
-package com.rik.MockProject_N1.controller;
+package com.rik.MockProject_N1.controller.admin;
 
-import com.rik.MockProject_N1.dto.LoginUser;
-import com.rik.MockProject_N1.dto.UserDTO;
 import com.rik.MockProject_N1.model.User;
 import com.rik.MockProject_N1.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,51 +8,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
 @RequestMapping(value = "/")
-public class UserController {
-
+public class AdminController {
     @Autowired
     UserServiceImpl userService;
 
-    //tạo đường dẫn mặc định cho home
-    @GetMapping("/home")
-    public String viewHomePage() {
-        return "index";
-    }
-
-    @PostMapping("/saveUser")
-    public String saveUser(@ModelAttribute("user") UserDTO userDTO, Model model) {
-        userService.saveUser(userDTO);
-        UserDTO user = new UserDTO();
-        model.addAttribute("user", user);
-        LoginUser loginUser = new LoginUser();
-        model.addAttribute("userLogin", loginUser);
-        return "account";
-    }
-
-    @GetMapping("/login")
-    public String login(Model model) {
-        UserDTO userDTO = new UserDTO();
-        model.addAttribute("user", userDTO);
-        LoginUser loginUser = new LoginUser();
-        model.addAttribute("userLogin", loginUser);
-        return "account";
-    }
-
-    @PostMapping("/login")
-    public String loginWeb(@ModelAttribute("userLogin") LoginUser loginUser, HttpServletRequest request) {
-        User user = userService.findByUsernameAndPassword(loginUser.getUsername(), loginUser.getPassword());
-        HttpSession httpSession = request.getSession();
-        if (user != null) {
-            httpSession.setAttribute("login", user);
-            return "redirect:home";
-        }
-        return "account";
+    @GetMapping(value = "/admin")
+    public String viewAdminHomePage(){
+        return "admin/index";
     }
 
     @GetMapping(value = "/admin/users")
